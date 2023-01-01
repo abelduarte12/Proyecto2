08c2b133e422e8ae1bf928375e784dd17e1c1deb
abel <abel.vilca.1212@gmail.com>
2023-01-01 17:04:48 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         /*   int horastrabajadas;
+            float costoHora;
+            float sueldo;
+            String linea;
+
+            Console.WriteLine("Ingrese horas trabajadas: ");
+            linea= Console.ReadLine();
+            horastrabajadas = int.Parse(linea);
+
+            Console.WriteLine("Ingrese el pago por hora: ");
+            linea= Console.ReadLine();
+            costoHora= float.Parse(linea);
+
+            sueldo= horastrabajadas* costoHora;
+            Console.WriteLine("el sueldo total del operario es: " + sueldo);
+            Console.ReadKey();
+         */
+
+            
+
+        }
+    }
+}
